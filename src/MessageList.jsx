d5e832c286e10d0612c868ebb3e948fa47e51985
Brandon Day <brandontday7@gmail.com>
2017-10-02 19:55:17 +0000

    got the main page rendered using react

---
+++ src/MessageList.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <main className="messages">
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </main>
+    );
+  }
+}
+export default MessageList;
+
